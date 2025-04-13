8944d12693c4cb5547aa02f91b71ac44701ab9a2
Mibrahim9400 <mohamadibrahim9400@gmail.com>
2025-04-13 18:13:21 +0100

    Delete DataBricks Notebook/Data_pipeline directory

--- DataBricks Notebook/Data_pipeline/input_output.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-# Load database credentials from a YAML file
-def load_db_credentials():
-    with open('db_creds.yaml', 'r') as file:
-        creds = yaml.load(file, Loader=yaml.FullLoader)
-    return creds
